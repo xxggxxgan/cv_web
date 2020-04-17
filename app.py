31569608d1b5ce07086612ace29b9c26dae8d46b
xxggxxgan <xxggxxgan@gmail.com>
2020-04-16 23:21:44 -0400

    sss

--- app.py
+++ app.py
@@ -57,8 +57,8 @@ def upload():
 
 @app.route('/func', methods=['GET', 'POST'])
 def runapp():
-    result = json.dumps(realtime.hh("static/images/sbb.png"),ensure_ascii=False)
-    return result
+    #result = json.dumps(realtime.hh("static/images/sbb.png"),ensure_ascii=False)
+    return "sfsafassafsad"
 
 
 
