3ce0642153b46ed3e850e716274cce9d77a31c79
xxggxxgan <xxggxxgan@gmail.com>
2020-04-18 22:57:44 -0400

    d

--- app.py
+++ app.py
@@ -15,16 +15,15 @@ import json
  
 from datetime import timedelta
 
-bb = realtime.hh("xiaoxingan.jpg")
-#print(bb)
 
+"""
 #设置允许的文件格式
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'bmp'])
  
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
  
-
+"""
 
 
 app = Flask(__name__)
